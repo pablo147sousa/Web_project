0beb74fe45ec9d43b79f71fc360890885e502892
Pablo Sousa <pablo147sousa@hotmail.com>
2024-06-06 15:12:36 +0100

    Fix template paths from Artigos App

--- apps/artigos/views.py
+++ apps/artigos/views.py
@@ -10,7 +10,7 @@ def index_view(request):
     context = {
         'artigos': artigos,
     }
-    return render(request, "index.html", context)
+    return render(request, "artigos/index.html", context)
 
 def article_view(request, article_id):
     article = get_object_or_404(Artigo, id=article_id)
@@ -21,17 +21,17 @@ def article_view(request, article_id):
         'comments': comments,
         'rating': rating,
     }
-    return render(request, "article.html", context)
+    return render(request, "artigos/article.html", context)
 
 
 def autor_view(request, autor_id):
     autor = get_object_or_404(Autor, id=autor_id)
-    artigos = autor.artigos.all()
+    artigos = autor.artigos.all() # type: ignore
     context = {
         'autor': autor,
         'artigos': artigos,
     }
-    return render(request, "autor.html", context)
+    return render(request, "artigos/autor.html", context)
 
 # Manipulação Autores
 
@@ -43,7 +43,7 @@ def new_author_view(request):
         return redirect('artigos:index')
 
     context = {'form': form}
-    return render(request, 'novo_autor.html', context)
+    return render(request, 'artigos/novo_autor.html', context)
 
 def edita_autor_view(request, autor_id):
     autor = Autor.objects.get(id=autor_id)
@@ -52,12 +52,12 @@ def edita_autor_view(request, autor_id):
         form = AutorForm(request.POST or None, instance=autor)
         if form.is_valid():
             form.save()
-            return redirect('artigos:autor',autor.id)
+            return redirect('artigos:autor',autor.id) # type: ignore
     else:
         form = AutorForm(instance=autor)  # cria formulário com dados da instância autor
 
     context = {'form': form, 'autor':autor}
-    return render(request, 'edita_autor.html', context)
+    return render(request, 'artigos/edita_autor.html', context)
 
 def apaga_autor_view(request, autor_id):
     autor = Autor.objects.get(id=autor_id)
@@ -74,10 +74,10 @@ def novo_artigo_view(request, autor_id):
         livro = form.save(commit=False)
         livro.autor = autor
         livro.save()
-        return redirect('artigos:autor',autor.id)
+        return redirect('artigos:autor',autor.id) # type: ignore
 
     context = {'form': form}
-    return render(request, 'novo_artigo.html', context)
+    return render(request, 'artigos/novo_artigo.html', context)
 
 def edit_article_view(request, article_id):
     article = Artigo.objects.get(id=article_id)
@@ -86,12 +86,12 @@ def edit_article_view(request, article_id):
         form = ArtigoForm(request.POST or None, request.FILES,instance=article)
         if form.is_valid():
             form.save()
-            return redirect('artigos:article',article.id)
+            return redirect('artigos:article',article.id) # type: ignore
     else:
         form = ArtigoForm(instance=article)  # cria formulário com dados da instância artigo
 
     context = {'form': form, 'article':article}
-    return render(request, 'edita_artigo.html', context)
+    return render(request, 'artigos/edita_artigo.html', context)
 
 def apaga_artigo_view(request, article_id):
     artigo = Artigo.objects.get(id=article_id)
